3ee459d4869a9fe89c89d6475c9468997be82178
AndreaGaggia <gaggiandrea@gmail.com>
2021-03-17 14:48:39 +0100

    style in admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -61,7 +61,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                                 document.getElementById('logout-form').submit();">
+                                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -77,7 +77,7 @@
         </nav>
 
         <main class="container">
-            <nav id="navbar-admin" class="navbar navbar-light bg-light px-0">
+            <nav id="navbar-admin" class="navbar navbar-light bg-transparent px-0">
                 <ul class="nav">
                     <li class="nav-item">
                         {{-- questo punterà all'index --}}
