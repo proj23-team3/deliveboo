d4023b34b5d39c71e18f4e3658ef8adbbc4fe5bd
mirko-droid <mirko.saponaro91@gmail.com>
2021-03-19 17:18:06 +0100

    overlayJumbotron

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,22 +15,19 @@
 
 <body>
     <div id="app">
-        <div class="container-fluid bg-primary overlay">
+        <div class="container-fluid bg-primary">
             <div class="container">
                 <nav class="navbar navbar-expand-lg navbar-light ">
                     <a class="navbar-brand" href="#">
                         <img src="{{ asset('img/logo_white.png') }}" alt="" height="34">
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item dropdown btn btn-light btn-sm">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Collabora con noi
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
