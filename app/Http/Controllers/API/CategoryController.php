a020005ae7c87850ab20cfa3a49643e445385f8c
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-03-21 13:09:55 +0100

    create api categoryController

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'response' => Category::all(),
+        ]);
+    }
+}
