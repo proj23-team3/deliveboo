b97f27fb4c1911b537dda1b0dfa685f4802b9958
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-03-23 12:27:42 +0100

    enhanced homepage

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,77 +1,115 @@
 @extends('layouts.guest')
 @section('content')
-<header>
-    <div class="jumbotron bg-secondary overlay">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <h1 class="purple font-weight-bold">I piatti che ami, a domicilio.</h1>
-                </div>
-                <div class="col-md-6">
-                    <img class="rider img-responsive" src="{{ asset('img/campaign.5a.svg') }}" alt="">
-                    <div class="hashtag">
-                        <h3 class="text-center p-3 font-weight-bolder">#aCasaTuaConDeliveboo</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
-<main>
-        <section>
-        <div class="container">
-            <h2 class="font-weight-bolder text-center">Ordina subito</h2>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card shadow my-4 p-5">
-                        <img class="card-img img-responsive" src="{{ asset('img/Chose.gif') }}" alt="Card image" height="200">
-                        <h4 class="card-text text-center">
-                            Scegli i tuoi piatti dal nostro menù
-                        </h4>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card shadow my-4 p-5">
-                        <img class="card-img img-responsive" src="{{ asset('img/Restaurant.gif') }}" alt="Card image" height="200">
-                        <h4 class="card-text text-center">
-                            Ordina dal tuo ristorante preferito
-                        </h4>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card shadow my-4 p-5">
-                        <img class="card-img img-responsive" src="{{ asset('img/Delivery.gif') }}" alt="Card image" height="200">
-                        <h4 class="card-text text-center">
-                            I tuoi piatti arriveranno al volo
-                        </h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="selection">
-        <div class="container">
-            <h2 class="font-weight-bolder">La selezione di Deliveboo</h2>
-            <div class="row">
-                @foreach ($categories as $category)
-                <div class="col-md-3">
-                    <a href="{{ route('categories.show', ['category' => $category->id]) }}">
-                        <div class="card my-2">
-                            <img class="card-img img-responsive" src="{{ asset($category->cat_img) }}" alt="Card image" height="200">
-                            <div class="card-img-overlay d-flex justify-content-center align-items-center">
-                                <h2 class="card-text text-center text-white font-weight-bolder">
-                                    {{ $category->name }}
-                                </h2>
+    <header>
+        <div class="jumbotron bg-secondary overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 d-flex flex-column justify-content-around">
+                        <h1 class="purple font-weight-bold mb-5">I piatti che ami, a domicilio.</h1>
+                        <!-- Carousel -->
+                        <div id="carousel1_indicator" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel1_indicator" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel1_indicator" data-slide-to="1"></li>
+                                <li data-target="#carousel1_indicator" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner rounded">
+                                <div class="carousel-item active">
+                                    <img class="d-block img-fluid" src="{{ asset('img/promo_masterchef.jpg') }}"
+                                        alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block img-fluid" src="{{ asset('img/promo_averna.jpg') }}"
+                                        alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block img-fluid" src="{{ asset('img/promo_cameo.jpg') }}"
+                                        alt="Third slide">
+                                </div>
                             </div>
-                            <p class="card-text">
-                                {{ $category->description }}
-                            </p>
+                            <a class="carousel-control-prev" href="#carousel1_indicator" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carousel1_indicator" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
-                    </a>
+                        <!-- /Carousel -->
+                    </div>
+                    <div class="col-md-6 d-flex flex-column justify-content-around">
+                        <img class="rider img-fluid mt-4" src="{{ asset('img/campaign.5a.svg') }}" alt="">
+                        <div class="hashtag">
+                            <h3 class="text-center p-3 font-weight-bolder rounded">#aCasaTuaConDeliveboo</h3>
+                        </div>
+                    </div>
                 </div>
-                @endforeach
             </div>
         </div>
-    </section>
-</main>
+    </header>
+    <main>
+        <section class="ordernow p-5">
+            <div class="container">
+                <h2 class="font-weight-bolder text-center">Ordina subito!</h2>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card shadow my-4 p-5">
+                            <img class="card-img img-fluid" src="{{ asset('img/Chose.gif') }}" alt="Card image"
+                                height="200">
+                            <h4 class="card-text text-center font-weight-bolder">
+                                Scegli i tuoi piatti dal nostro menù
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card shadow my-4 p-5">
+                            <img class="card-img img-fluid" src="{{ asset('img/Restaurant.gif') }}" alt="Card image"
+                                height="200">
+                            <h4 class="card-text text-center font-weight-bolder">
+                                Ordina dal tuo ristorante preferito
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card shadow my-4 p-5">
+                            <img class="card-img img-fluid" src="{{ asset('img/Delivery.gif') }}" alt="Card image"
+                                height="200">
+                            <h4 class="card-text text-center font-weight-bolder">
+                                I tuoi piatti arriveranno al volo!
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="favourite bg_rose p-5">
+            <div class="container">
+                <h2 class="font-weight-bolder">La selezione di Deliveboo</h2>
+                <div class="row">
+                    @foreach ($categories as $category)
+                        <div class="col-md-4">
+                            <a class="p-1" href="{{ route('categories.show', ['category' => $category->id]) }}">
+                                <div class="card card_container">
+                                    <img class="img-fluid card-img" src="{{ asset($category->cat_img) }}"
+                                        alt="{{ $category->name }}">
+                                    <div class="card-img-overlay d-flex">
+                                        <div class="my-auto mx-auto text-center category-name shadowed">
+                                            <h2 class="card-title font-weight-bolder text-white ">
+                                                {{ $category->name }}
+                                            </h2>
+                                        </div>
+                                        <div class="my-auto mx-auto text-center category-description shadowed">
+                                            <h4 class="card-title font-weight-bolder text-white">
+                                                {{ $category->description }}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+    </main>
 @endsection
