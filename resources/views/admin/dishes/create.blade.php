b761246aaecfe2a6da513bff9d5a11321d46ee4b
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-03-25 12:48:07 +0100

    sistemate un botto di cose

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -2,57 +2,60 @@
 
 @section('content')
 
-    <h1>Crea Il tuo Piatto</h1>
+    <h1 class="mt-5 font-weight-bolder">Crea il tuo piatto</h1>
 
-    <div class="container">
-        <form method="POST" action="{{ route('admin.dishes.store') }}" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group row">
-                <label for="dish_name" class="col-sm-1-12 col-form-label"></label>
-                <div class="col-sm-1-12">
-                    <input type="text" class="form-control" name="dish_name" id="dish_name" placeholder="Dish Name">
-                </div>
-            </div>
-            <div class="form-group row">
 
-                <label for="dish_ingredients" class="col-sm-1-12 col-form-label"></label>
-                <div class="col-sm-1-12">
-                    <textarea name="dish_ingredients" id="dish_ingredients" cols="30" rows="10"
-                        placeholder="Dish Ingredients"></textarea>
-                </div>
+    <form method="POST" action="{{ route('admin.dishes.store') }}" enctype="multipart/form-data">
+        @csrf
+        <hr>
+        <div class="form-group row">
+            <label for="dish_name" class="col-form-label col-md-12">Inserisci il nome del tuo piatto</label>
+            <div class="col-md-4">
+                <input type="text" class="form-control" name="dish_name" id="dish_name" placeholder="Nome del piatto">
             </div>
-            <div class="form-group row">
-                <label for="dish_price" class="col-sm-1-12 col-form-label"></label>
-                <div class="col-sm-1-12">
-                    <input type="number" step=".01 " class="form-control" name="dish_price" id="dish_price"
-                        placeholder="Dish Price">
-                </div>
-            </div>
-            <div class="form-group row">
-                <div class="form-check">
-                    <label class="form-check-label" for="is_visible">
-                        <input type="radio" class="form-check-input" name="is_visible" id="is_visible" value="1" checked>
-                        Visible
-                    </label>
-                </div>
-                <div class="form-check">
-                    <label class="form-check-label" for="is_invisible">
-                        <input type="radio" class="form-check-input" name="is_visible" id="is_invisible" value="1">
-                        Invisible
-                    </label>
-                </div>
-            </div>
+        </div>
+        <div class="form-group row">
 
-            <div class="form-group row">
-                <label for="dish_image"
-                    class="col-md-4 col-form-label text-md-right">{{ __('An image of your restaurant') }}</label>
-
-                <div class="col-md-6">
-                    <input type="file" name="dish_image" id="dish_image" class="form-control-file">
-                </div>
+            <label for="dish_ingredients" class="col-md-12 col-form-label">Inserisci la descrizione con gli ingredienti
+                del
+                tuo piatto</label>
+            <div class="col-md-12">
+                <textarea style="resize: none; padding: 10px; min-width: 100%; border: 1px solid lightgrey;"
+                    name="dish_ingredients" id="dish_ingredients" cols="50" rows="10"
+                    placeholder="Descrivi il tuo piatto..."></textarea>
             </div>
-            <button type="submit"> Create Dish</button>
-        </form>
-    </div>
+        </div>
+        <div class="form-group row">
+            <label for="dish_price" class="col-md-12 col-form-label">Inserisci il prezzo di vendita del tuo piatto</label>
+            <div class="col-md-4 mb-4">
+                <input type="number" step=".01 " class="form-control" name="dish_price" id="dish_price"
+                    placeholder="Prezzo in €">
+            </div>
+        </div>
+        <p>Indica se il tuo piatto è attualmente disponibile per l'acquisto</p>
+        <div class="form-group row">
+            <div class="form-check col-md-2 ml-3">
+                <label class="form-check-label" for="is_visible">
+                    <input type="radio" class="form-check-input" name="is_visible" id="is_visible" value="1" checked>
+                    Disponibile
+                </label>
+            </div>
+            <div class="form-check col-md-2">
+                <label class="form-check-label" for="is_invisible">
+                    <input type="radio" class="form-check-input" name="is_visible" id="is_invisible" value="1">
+                    Esaurito
+                </label>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="dish_image"
+                class="col-md-12 col-form-label mt-4 mb-2">{{ __('Inserisci la foto del tuo piatto') }}</label>
+            <div class="col-md-5">
+                <input type="file" name="dish_image" id="dish_image" class="form-control-file">
+            </div>
+        </div>
+        <button class="btn btn-primary mt-3" type="submit">Aggiungi il tuo piatto!</button>
+    </form>
+
 
 @endsection
--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,58 +1,64 @@
 @extends('layouts.admin')
 
 @section('content')
-    <a href="{{ route('admin.dishes.create') }}" class="btn btn-outline-info my-4">Aggiungi un nuovo piatto</a>
-    <div class="row">
-        @foreach ($dishes as $dish)
-        <div class="col-md-3 pr-4">
-            <div class="card">
-                <img class="card-img-top"
-                    src="{{ str_contains($dish->dish_image, 'dishes') ? asset('storage/' . $dish->dish_image) : asset('img/default_dish.svg') }}"
-                    width="250" alt="Immagine piatto">
-                <div class="card-body">
-                    <h5 class="card-title text-uppercase"><strong>{{ $dish->dish_name }}</strong></h5>
-                    <div class="mb-4">
-                        <small>Ingredienti:</small>
-                        <p class="card-text">{{ $dish->dish_ingredients }}</p>
-                    </div>
-                    <a href="{{ route('admin.dishes.edit', ['dish' => $dish->id]) }}" class="btn btn-info">Modifica il
-                        piatto</a>
-                        <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-danger mt-1" data-toggle="modal" data-target="#delete-{{$dish->id}}">
-                        Elimina questo piatto
-                    </button>
-    
-                    <!-- Modal -->
-                    <div class="modal fade" id="delete-{{$dish->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                <p>Vuoi cancellare : {{$dish->dish_name}}</p>
-                                </div>
-                                <div class="modal-footer">       
-                                    <form action="{{ route('admin.dishes.destroy', ['dish' => $dish->id]) }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="btn btn-danger mt-1">Elimina questo piatto</button>
-                                    </form>
+    <div class="container">
+        <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary my-4">Aggiungi un nuovo piatto</a>
+        <div class="row">
+            @foreach ($dishes as $dish)
+                <div class="col-md-6 mb-5">
+                    <div class="card">
+                        <div class="image_wrapper">
+                            <img class="card-img-top dish_image"
+                                src="{{ str_contains($dish->dish_image, 'dishes') ? asset('storage/' . $dish->dish_image) : asset('img/default_dish.svg') }}"
+                                alt="Immagine piatto">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-uppercase"><strong>{{ $dish->dish_name }}</strong></h5>
+                            <div class="mb-4">
+                                <small>Ingredienti:</small>
+                                <p class="card-text">{{ $dish->dish_ingredients }}</p>
+                            </div>
+                            <a href="{{ route('admin.dishes.edit', ['dish' => $dish->id]) }}"
+                                class="btn btn-primary mr-2 mt-2">Modifica il
+                                piatto</a>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-danger mr-2 mt-2" data-toggle="modal"
+                                data-target="#delete-{{ $dish->id }}">
+                                Elimina questo piatto
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="delete-{{ $dish->id }}" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Vuoi cancellare : {{ $dish->dish_name }}</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <form action="{{ route('admin.dishes.destroy', ['dish' => $dish->id]) }}"
+                                                method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-danger mt-1">Elimina questo piatto</button>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
                     </div>
-                    
                 </div>
-            </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
-    
+
 
 @endsection
-
-
