046c24a009863b2febbea44e0dddae43ffc0cfbb
AndreaGaggia <gaggiandrea@gmail.com>
2021-03-25 16:24:18 +0100

    new checkout view

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -2,24 +2,51 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Order;
 use Braintree;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PaymentsController extends Controller
 {
-    public function process(Request $request )
+    public function process(Request $request)
     {
         $payload = $request->input('payload', false);
         $nonce = $payload['nonce'];
         $tot = $payload['tot'];
-        
+
         $status = Braintree\Transaction::sale([
-        'amount' => $tot,
-        'paymentMethodNonce' => $nonce,
-        'options' => [
-            'submitForSettlement' => True
-        ]
+            'amount' => $tot,
+            'paymentMethodNonce' => $nonce,
+            'options' => [
+                'submitForSettlement' => true,
+            ],
         ]);
-         return response()->json($status);
+        return response()->json($status);
+    }
+
+    public function ajaxRequestPost(Request $request)
+    {
+        Log::info($request);
+
+        $validated = $request->validate([
+            'customer_name' => 'required',
+            'customer_email' => 'required',
+            'customer_telephone' => 'required',
+            'customer_address' => 'required',
+            'delivery_date' => 'required',
+            'delivery_time' => 'required',
+            'amount' => 'required',
+            'user_id' => 'required',
+        ]);
+
+        Order::create($validated);
+
+        $response = array(
+            'status' => 'success',
+            'name' => $request->customer_name,
+        );
+        return response()->json($response);
+
     }
 }
--- resources/views/guest/payment.blade.php
+++ resources/views/guest/payment.blade.php
@@ -1,56 +1,186 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<!DOCTYPE html>
+
+<html>
+
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Braintree-Demo</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-  <script src="https://js.braintreegateway.com/web/dropin/1.8.1/js/dropin.min.js"></script>
+    <title>Deliveboo checkout</title>
 
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    {{-- for post req --}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+    {{-- for braintree functionality --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/dropin/1.8.1/js/dropin.min.js"></script>
+
+    {{-- style --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
-<body>
- 
-  <div class="container">
-     <div class="row">
-       <div class="col-md-8 col-md-offset-2">
-         <div id="dropin-container"></div>
-         <button id="submit-button">Request payment method</button>
-       </div>
-     </div>
-  </div>
-  <script>
-    var button = document.querySelector('#submit-button');
 
-    braintree.dropin.create({
-      authorization: "{{ Braintree\ClientToken::generate() }}",
-      container: '#dropin-container'
-    }, function (createErr, instance) {
-      let carrellino = JSON.parse(localStorage.getItem('carrello'));
-            let tot = 0;
-            if(localStorage.carrello){
-              carrellino.forEach(item=>{
+<body>
+
+    <div class="container">
+
+        <p>CHECKOUT</p>
+
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="name">Nome:</label>
+                <input type="text" name="name" id="name" class="form-control" placeholder="Nome" required="">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" class="form-control" placeholder="Email" required="">
+            </div>
+
+            <div class="form-group">
+                <label for="telephone">Telefono:</label>
+                <input type="text" name="telephone" id="telephone" class="form-control" placeholder="Telefono"
+                    required="">
+            </div>
+
+            <div class="form-group">
+                <label for="address">Indirizzo di consegna:</label>
+                <input type="text" name="address" id="address" class="form-control" placeholder="Indirizzo di consegna"
+                    required="">
+            </div>
+
+            <div class="form-group">
+                <label for="delivery_date">Data di consegna:</label>
+                <input type="date" name="delivery_date" id="delivery_date" class="form-control" required="">
+            </div>
+
+            <div class="form-group">
+                <label for="delivery_time">Orario di consegna:</label>
+                <input type="time" name="delivery_time" id="delivery_time" class="form-control" required="">
+            </div>
+
+            <div class="form-group">
+                <button class="btn btn-success btn-submit">Conferma i dati</button>
+            </div>
+        </form>
+
+        {{-- braintree dropin --}}
+        <div class="col-md-8 col-md-offset-2">
+            <div id="dropin-container"></div>
+            <button id="submit-button" class="d-none">Request payment method</button>
+        </div>
+
+    </div>
+
+</body>
+
+<script>
+    $.ajaxSetup({
+
+        headers: {
+
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+
+        }
+
+    });
+
+    $(".btn-submit").click(function(e) {
+
+        e.preventDefault();
+
+        var name = $("input[name=name]").val();
+
+        var email = $("input[name=email]").val();
+
+        var telephone = $("input[name=telephone]").val();
+        // console.log(telephone);
+
+        var address = $("input[name=address]").val();
+
+        var delivery_date = $("input[name=delivery_date]").val();
+
+        var delivery_time = $("input[name=delivery_time]").val();
+
+        let carrellino = JSON.parse(localStorage.getItem('carrello'));
+        let risto_id = 0;
+        let tot = 0;
+        if (localStorage.carrello) {
+            carrellino.forEach(item => {
                 let subtotal = item.qty * item.price;
                 tot += subtotal;
-            } )
+                risto_id = item.risto_id;
+            })
+        }
+        // console.log(tot, risto_id);
+
+        var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+        $.ajax({
+
+            type: 'POST',
+
+            url: '/ajaxRequest',
+
+            data: {
+                _token: CSRF_TOKEN,
+                customer_name: name,
+                customer_email: email,
+                customer_telephone: telephone,
+                customer_address: address,
+                delivery_date: delivery_date,
+                delivery_time: delivery_time,
+                amount: tot,
+                user_id: risto_id,
+            },
+
+            success: function(response) {
+                // entra in gioco braintree
+                var button = document.getElementById('submit-button');
+                button.classList.remove('d-none');
+                braintree.dropin.create({
+                    authorization: "{{ Braintree\ClientToken::generate() }}",
+                    container: '#dropin-container'
+                }, function(createErr, instance) {
+                    // let carrellino = JSON.parse(localStorage.getItem('carrello'));
+                    // let tot = 0;
+                    // if (localStorage.carrello) {
+                    //     carrellino.forEach(item => {
+                    //         let subtotal = item.qty * item.price;
+                    //         tot += subtotal;
+                    //     })
+                    // }
+
+                    button.addEventListener('click', function() {
+                        instance.requestPaymentMethod(function(err, payload) {
+                            payload.tot = tot;
+                            $.get('{{ route('payment.process') }}', {
+                                payload
+                            }, function(response) {
+                                console.log(response);
+                                if (response.success) {
+                                    alert('Payment successful!');
+                                } else {
+                                    alert('Payment failed');
+                                }
+                            }, 'json');
+                        });
+                    });
+                });
+
+            },
+
+            error: function(err) {
+                console.log(err.responseJSON.errors);
+                // logica errori
             }
-            
-      button.addEventListener('click', function () {
-        instance.requestPaymentMethod(function (err, payload) {
-          payload.tot = tot;
-          $.get('{{ route('payment.process') }}', {payload}, function (response) {
-              console.log(response);
-            if (response.success) {
-              alert('Payment successfull!');
-            } else {
-              alert('Payment failed');
-            }
-          }, 'json');
+
         });
-      });
     });
-  </script>
-</body>
+
+</script>
+
 </html>
