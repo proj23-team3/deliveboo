136bbf1905f98c8aab36011e326652138464a7db
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-03-25 19:55:01 +0100

    add OrderController and  route

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $orders = Auth::user()->orders;
+        return view('admin.orders.index', compact('orders'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Order $order)
+    {
+        return view('admin.orders.show', compact('order'));
+    }
+}
