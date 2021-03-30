78d44c6682ba52d7183a2a80edde7efd65952a83
AndreaGaggia <gaggiandrea@gmail.com>
2021-03-30 14:21:15 +0200

    modify register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,15 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
+                    <div class="card-header">{{ __('Form di registrazione') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                             @csrf
 
                             <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                                <label for="name"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Nome del ristorante') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
@@ -28,7 +29,7 @@
 
                             <div class="form-group row">
                                 <label for="email"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo email') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
@@ -61,7 +62,7 @@
 
                             <div class="form-group row">
                                 <label for="password-confirm"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
@@ -71,7 +72,7 @@
 
                             <div class="form-group row">
                                 <label for="address"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Restaurant Address') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo del ristorante') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="address" type="text" class="form-control" name="address" required
@@ -81,7 +82,7 @@
 
                             <div class="form-group row">
                                 <label for="vat_number"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Restaurant VAT Number') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('P.IVA Ristorante') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="vat_number" type="text" class="form-control" name="vat_number" required
@@ -91,7 +92,7 @@
 
                             <div class="form-group row">
                                 <label for="shipping_costs"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Shipping costs') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Costi di consegna') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="shipping_costs" type="number" step=".01" class="form-control"
@@ -101,7 +102,7 @@
 
                             <div class="form-group row">
                                 <label for="cover"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('An image of your restaurant') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Un\'immagine del ristorante') }}</label>
 
                                 <div class="col-md-6">
                                     <input type="file" name="cover" id="cover" class="form-control-file">
@@ -109,7 +110,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="categories" class="col-md-4 col-form-label text-md-right">Categories</label>
+                                <label for="categories" class="col-md-4 col-form-label text-md-right">Tipo di cucina</label>
                                 <div class="col-md-6">
                                     <select name="categories[]" id="categories" class="form-control" multiple>
                                         @if ($categories)
@@ -127,7 +128,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Register') }}
+                                        {{ __('Registrati') }}
                                     </button>
                                 </div>
                             </div>
