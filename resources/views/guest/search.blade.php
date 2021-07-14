284731c16d5e979c225a190f4f58f2853f7bfa91
antoniolovotrico <antonio.lovotrico@gmail.com>
2021-07-14 19:54:22 +0200

    publish to heroku

--- resources/views/guest/search.blade.php
+++ resources/views/guest/search.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.guest')
 @section('content')
-    <search-component :choose_cat='@json($category->id)' :rests='@json($restaurants)'> />
+    <search-component :choose_cat='@json($category->id)' :rests='@json($restaurants)' />
     @endsection
